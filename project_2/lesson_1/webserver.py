205331b7dded925b6f82b8622a2e11030967ddae
Jay Shah <j24shah@gmail.com>
2019-05-08 23:13:06 -0400

    doc: Updated for lesson 2.
    This focuses on modifying the webserver file to display all restaurant
    files. Also added in a new url "/restaurant" - when there it will
    dipslay all restaurant names and add "links" to Edit the name and delete
    the name. Edit and Delete links do not work at the moment.

--- project_2/lesson_1/webserver.py
+++ project_2/lesson_1/webserver.py
@@ -6,6 +6,18 @@ This file acts as the websever to our webapp.
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+# Importing out need modules from restaurant databas.
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Connecting to the database.
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 class webserverHandler(BaseHTTPRequestHandler):
     """
     Extends BaseHTTPRequestHandler class. 
@@ -16,6 +28,28 @@ class webserverHandler(BaseHTTPRequestHandler):
         GET Method for the server.
         """
         try:
+            
+            if self.path.endswith("/restaurant"):
+                # Gathering all resturant name from db.
+                restaurants = session.query(Restaurant).all()
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                
+                #Printing the output.
+                output = ''
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name + "</br>"
+                    # output += "</br>"
+                    output += "<a href='#'> Edit </a></br>"
+                    output += "<a href='#'> Delete</a></br>"
+                    output += "</br>"
+
+                output += "</body></html>"
+                self.wfile.write(output)
+                return 
+
             if self.path.endswith("/hello"): # if the url ends with /hello do the following.
                 self.send_response(200) # sends a response 200 for success.
                 self.send_header('Content-type', 'text/html') # returns some headers.
